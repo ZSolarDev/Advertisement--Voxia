44d41eaa4144258ce6250190f3891fb73e930305
fgdg <zsolardevofficial@gmail.com>
2025-03-15 18:16:45 -0400

    here we are

--- app.js
+++ app.js
@@ -1,34 +1,6 @@
-var rcon = require('rcon/node-rcon.js');
 const express = require("express");
-
-
 const app = express();
-const PORT = process.env.PORT;
-const RCON_IP = '51.222.147.157';
-const RCON_PORT = 8011;
-const RCON_PASSWORD = '819VDe1x3l201';
-
-var rconAuthenticated = false;
 var messages = ["e", "e2", "ee3"];
-
-var conn = new rcon(RCON_IP, RCON_PORT, RCON_PASSWORD);
-console.log("loading...");
-conn.on('auth', function() {
-        console.log("RCON Authenticated successfully");
-        rconAuthenticated = true;
-    }).on('response', function(str) {
-        console.log("Response: " + str);
-    }).on('error', function(err) {
-        console.log("Error: " + err);
-        rconAuthenticated = false;
-        conn.connect();
-    }).on('end', function() {
-        console.log("Connection closed");
-        rconAuthenticated = false;
-        conn.connect();
-    }
-);
-conn.connect();
 
 app.get("/", (req, res) => {
     res.send(messages);
@@ -41,15 +13,6 @@ app.post("/", (req, res) => {
     res.status(200).send("OK");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+app.listen(3000, () => {
+    console.log(`Server is listening on port 3000`);
 });
-
-function sendAnnouncment() {
-    if (rconAuthenticated) {
-        conn.send('tellraw @a {"text": "' + messages[Math.floor(Math.random() * messages.length)] + '"}');
-        console.log("Announcement sent");
-    }
-}
-sendAnnouncment();
-setInterval(sendAnnouncment, 30000);
